086bedd2465412766372fbcbcb60028ffc209f1c
Abhay Chaudhary <166979908+abhaychaudhary18@users.noreply.github.com>
2026-06-24 15:48:33 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (78.33%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -6,15 +6,53 @@
         
 //     }
 // };
+
+
 class Solution{
     public:
-    void sortColors(vector<int>& nums){
-        int low=0,mid=0,high=nums.size()-1;
+    void sortColors(vector<int>&nums){
+        int low=0;
+        int mid=0;
+        int high=nums.size()-1;
         while(mid<=high){
             if(nums[mid]==0)swap(nums[mid++],nums[low++]);
             else if(nums[mid]==1)mid++;
             else swap(nums[mid],nums[high--]);
-
         }
+
     }
 };
+
+// class Solution{
+//     public:
+//     void sortColors(vector<int>&nums){
+//         int n = nums.size();
+//         int count0s=0;
+//         int count1s=0;
+//         int count2s=0;
+//         for(int i=0;i<n;i++){
+//             if(nums[i]==0){
+//                 count0s += 1;
+
+//             }else if(nums[i]==1){
+//                 count1s+=1;
+
+//             }else{
+//                 count2s +=1;
+//             }
+//         }
+//         for(int i=0;i<n;i++){
+//             if(count0s>0){
+//                 nums[i]=0;
+//                 count0s--;
+//             }else if(count1s>0){
+//                 nums[i]=1;
+//                 count1s--;
+//             }else if(count2s>0){
+//                 nums[i]=2;
+//                 count2s--;
+//             }
+//         }
+
+//   }
+// };
