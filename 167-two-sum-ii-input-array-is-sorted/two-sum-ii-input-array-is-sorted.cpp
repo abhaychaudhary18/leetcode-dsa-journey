0648b24257dbede9a8241640d3b415c97e9240a2
Abhay Chaudhary <166979908+abhaychaudhary18@users.noreply.github.com>
2026-06-25 15:27:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (32.98%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -17,17 +17,32 @@
 class Solution{
     public:
     vector<int>twoSum(vector<int>& nums, int target){
-        int left=0;
-        int right = nums.size()-1;
-        while(left<right){
-            int sum = nums[left]+nums[right];
-            if(sum==target)
-            return {left+1,right+1};
-            else if(sum<target)
-            left++;
-            else
-            right--;
+        map<int,int> mp;
+        for(int i=0;i<nums.size(); i++){
+            int temp = target-nums[i];
+            if(mp.find(temp)!= mp.end()){
+                return {mp[temp]+1,i+1};
+            }
+            mp[nums[i]]=i;
         }
         return {};
     }
 };
+
+// class Solution{
+//     public:
+//     vector<int>twoSum(vector<int>&nums, int target){
+//         int left=0;
+//         int right = nums.size()-1;
+//         while(left<right){
+//             int sum = nums[left]+nums[right];
+//             if(sum==target)
+//             return{left+1,right+1};
+//             else if(sum<right)
+//             left++;
+//             else
+//             right--;
+//         }
+//         return{};
+//     }
+// }; 
