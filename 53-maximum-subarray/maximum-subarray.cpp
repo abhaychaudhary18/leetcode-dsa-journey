7dfb0dc54a34e792d4bac7f59f41b70d4ea06962
Abhay Chaudhary <166979908+abhaychaudhary18@users.noreply.github.com>
2026-06-29 21:00:52 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (51.83%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,12 @@
+class Solution{
+    public:
+    int maxSubArray(vector<int>&nums){
+        int curMax = nums[0];
+        int globalMax = nums[0];
+        for(int i=1;i<nums.size();i++){
+            curMax = max(nums[i], curMax+nums[i]);// extend the old subArray
+            globalMax = max(globalMax, curMax);
+        }
+        return globalMax;
+    }
+};
