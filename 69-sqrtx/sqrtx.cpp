d097a7d2c41bcf2ddd8beab1fbf4393a52da6c20
Abhay Chaudhary <166979908+abhaychaudhary18@users.noreply.github.com>
2026-06-30 14:49:15 +0530

    Time: 4 ms (14.66%) | Memory: 8.6 MB (13.75%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,11 +1,31 @@
-class Solution {
-public:
-    int mySqrt(int x) {
-        long long i=0;
-        while(i*i<=x){
-            i++;
-        }
-        return i-1;
+// class Solution {
+// public:
+//     int mySqrt(int x) {
+//         long long i=0;
+//         while(i*i<=x){
+//             i++;
+//         }
+//         return i-1;
         
+//     }
+// };
+
+class Solution{
+    public:
+    int mySqrt(int x){
+        int low =1;
+        int high =x;
+        long long mid;
+        int ans=0;
+        while(low<=high){
+            mid = (high-low)/2+low;
+            if(mid*mid<=x){
+                ans = mid;
+                low = mid + 1;
+            }else {
+                high = mid-1;
+            }
+        }
+        return ans ;
     }
 };
