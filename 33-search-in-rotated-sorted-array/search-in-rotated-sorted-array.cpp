434d80129c3a280923b1f01a5717482952190e79
Abhay Chaudhary <166979908+abhaychaudhary18@users.noreply.github.com>
2026-07-01 12:55:12 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (99.93%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,22 +1,26 @@
-class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        int lo =0, hi = n-1;
-        while (lo < hi){
-            int mid = lo + (hi - lo) /2;
-            if(nums[mid] > nums.back()) lo = mid +1;
-            else hi = mid;
-        }
-        int rot =lo;
-        lo = 0, hi = n-1;
-        while(lo <= hi){
-            int mid = lo +(hi-lo)/2;
-            int real = (mid + rot)% n;
-            if(nums[real] == target)
-            return real;
-            if(nums[real] < target) lo =mid +1;
-            else hi = mid -1;
+class Solution{
+    public:
+    int search(vector<int>&nums, int target){
+        int left =0;
+        int right = nums.size()-1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(nums[mid]==target)
+            return mid;
+            if(nums[left]<= nums[mid]){
+                if(nums[left]<= target && target< nums[mid]){
+                    right = mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<target && target<= nums[right]){
+                    left = mid+1;
+                }else{
+                    right = mid -1;
+                }
+            }
         }
         return -1;
     }
