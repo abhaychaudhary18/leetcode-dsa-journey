3e071f23e0b0b6eb58a89f40b5fc1bcb5fc57e80
Abhay Chaudhary <166979908+abhaychaudhary18@users.noreply.github.com>
2026-07-05 14:28:47 +0530

    Time: 4 ms (23.37%) | Memory: 9.2 MB (94.71%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,39 +1,39 @@
-// class Solution{
-//     public:
-//     boolIsomorphic(String s, string t){
-//         unordered_map<char, char>m;
-//         for(int i=0;i<s.size();i++){
-//             if(m.count(s[i])){
-//                 if(m[s[i]] != t[i])
-//                 return false;
-//             }
-//             else{
-//                 for(auto x:m){
-//                     if(x.second == t[i])
-//                     return false;
-//                 }
-//                 m[s[i]]=t[i];
-//             }
-//         }
-//         return false;
-//     }
-// }
-
-
 class Solution{
     public:
-    bool isIsomorphic(string s, string t){
-        int m1[256] ={0};
-        int m2[256] ={0};
+    bool isIsomorphic(string s , string t){
+        unordered_map<char,char>m;
         for(int i=0;i<s.size();i++){
-            if(m1[s[i]] != m2[t[i]]){
+            if(m.count(s[i])){
+                if(m[s[i]]!= t[i])
                 return false;
-            }else{
-                m1[s[i]]=i+1;
-                m2[t[i]] = i+1;
+            }
+            else{
+                for(auto x:m){
+                    if(x.second == t[i])
+                    return false;
+                }
+                m[s[i]]=t[i];
             }
         }
         return true;
     }
 };
+
+
+// class Solution{
+//     public:
+//     bool isIsomorphic(string s, string t){
+//         int m1[256] ={0};
+//         int m2[256] ={0};
+//         for(int i=0;i<s.size();i++){
+//             if(m1[s[i]] != m2[t[i]]){
+//                 return false;
+//             }else{
+//                 m1[s[i]]=i+1;
+//                 m2[t[i]] = i+1;
+//             }
+//         }
+//         return true;
+//     }
+// };
 
