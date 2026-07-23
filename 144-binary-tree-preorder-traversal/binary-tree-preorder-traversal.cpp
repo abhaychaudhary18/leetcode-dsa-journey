022a83b4fc83ed841eb97de21b8cef79c782ef48
Abhay Chaudhary <166979908+abhaychaudhary18@users.noreply.github.com>
2026-07-23 15:27:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (94.56%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -9,20 +9,36 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-void helper(TreeNode* root, vector<int>& ans){
-    if(root== NULL)
-    return;
+// class Solution {
+// public:
+// void helper(TreeNode* root, vector<int>& ans){
+//     if(root== NULL)
+//     return;
+//     ans.push_back(root->val);
+//     helper(root->left,ans);
+//     helper(root->right,ans);
+// }
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         vector<int>ans;
+//         helper(root,ans);
+//         return ans;
+
+        
+//     }
+// };
+
+class Solution{
+    public:
+    void helper(TreeNode* root, vector<int>&ans){
+    if(root == NULL)
+    return ;
     ans.push_back(root->val);
     helper(root->left,ans);
     helper(root->right,ans);
 }
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        helper(root,ans);
-        return ans;
-
-        
-    }
+vector<int> preorderTraversal(TreeNode* root){
+    vector<int>ans;
+    helper(root,ans);
+    return ans;
+}
 };
