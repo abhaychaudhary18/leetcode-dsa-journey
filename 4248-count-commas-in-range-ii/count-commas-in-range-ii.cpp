1503188bf5c5fa489f881dd5b64df56ea3dd0666
Abhay Chaudhary <166979908+abhaychaudhary18@users.noreply.github.com>
2026-09-10 13:52:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (83.63%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -4,8 +4,7 @@ public:
     
     int64_t countCommas(auto& n) {
         int64_t k = 0;        
-        for (auto& p : pow) k += n >= p;
-        
+        for (auto& p : pow) k += n >= p;    
         return k * (n + 1) - (pow[k] - 1000) / 999;
     }
 };
